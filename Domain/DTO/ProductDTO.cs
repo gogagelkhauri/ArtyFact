f0d3bd828afbbc8ecbd74edbd5d334cb58f87a41
gogagelkhauri1@gmail.com <gogagelkhauri1@gmail.com>
2021-05-25 17:13:17 +0400

    validation1

--- Domain/DTO/ProductDTO.cs
+++ Domain/DTO/ProductDTO.cs
@@ -11,15 +11,22 @@ namespace Domain.DTO
     public class ProductDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public bool InStock { get; set; }
+        [Required]
         public string Description { get; set; }
         //[Required]
-        [DisplayFormat( NullDisplayText = "Choose Image" )]    
+        [DisplayFormat( NullDisplayText = "Choose Image" )]
+        [Required(ErrorMessage = "Pick an Image")]
+        [AllowedExtensions(new string[] { ".jpg", ".jpeg", ".png" })]
         public IFormFile ActualImage { get; set; }
         public string ImageURL { get; set; }
+
+        [Range(0, float.MaxValue, ErrorMessage = "Please enter valid float Number")]
         public float Price { get; set; }
         public int UserId { get; set; }
+        [Required]
         public int CategoryId { get; set; }
         public CategoryDTO Category { get; set; }
         #nullable enable
